5491ed0c0f9838facd9266ea353f345bea97c1db
anthonymnf <anthonymnf30@gmail.com>
2024-06-24 15:43:08 -0300

    :sparkles: feat: Trigonometria usando Python

---
+++ trigonometria_com_python.py
@@ -0,0 +1,10 @@
+import numpy as np
+angle = np.pi/6
+x1 = np.cos(angle)
+x2 = np.sin(angle)
+x3 = np.tan(angle)
+x4 = np.sqrt(5) #raiz quadrada
+print("cos(pi/6) =", x1 )
+print("sen(pi/6) =", x2 )
+print("tan(pi/6) =", x3)
+print("sqrt(pi/6) =", x4 )
